1638d05ca27730375a1ea048985fb323f0d78741
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-04-06 22:09:56 -0400

    no encryption bug fixed

--- config/database.js
+++ config/database.js
@@ -9,6 +9,12 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    options: {
+      ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
   },
