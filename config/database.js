a806b20ace0265fb3a1ca3271e4d19291286d005
Lucas Cardoso <lucascardoso@Lucass-MacBook-Pro.local>
2022-08-13 21:55:23 -0400

    update: database credentials by .env

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      host: env('DATABASE_HOST', process.env.REACT_APP_DATABASE_HOST),
+      port: env.int('DATABASE_PORT', process.env.REACT_APP_DATABASE_PORT),
+      database: env('DATABASE_NAME', process.env.REACT_APP_DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.REACT_APP_DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.REACT_APP_DATABASE_PASSWORD),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
